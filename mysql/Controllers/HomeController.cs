60cf7de514d73e517edc91fa0f1f8d869aec2be8
mperry5th <90285075+mperry5th@users.noreply.github.com>
2022-03-30 17:13:58 -0600

    Add project files.

---
+++ mysql/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using mysql.Models;
+
+namespace mysql.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BowlingDbContext _context { get; set; }
+
+        public HomeController(BowlingDbContext temp)
+        {
+            _context = temp;
+        }
+        public IActionResult Index(string team)
+        {
+            var coolData = _context.Bowlers
+                .Include(y => y.Team)
+                .Where(y => y.Team.TeamName == team || team == null)
+                .ToList();
+            return View(coolData);
+        }
+
+        [HttpGet]
+        public IActionResult AddBowler()
+        {
+            ViewBag.Teams = _context.teams.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddBowler(Bowler b)
+        {
+            _context.Add(b);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditBowler(int id)
+        {
+            ViewBag.Teams = _context.teams.ToList();
+            var bowler = _context.Bowlers.Single(y => y.BowlerID == id);
+
+            return View(bowler);
+        }
+        [HttpPost]
+        public IActionResult EditBowler(Bowler b)
+        {
+            _context.Update(b);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var bowler = _context.Bowlers.Single(y => y.BowlerID == id);
+            return View(bowler);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Bowler b)
+        {
+            var deleteBowler = _context.Bowlers.Single(y => y.BowlerID == b.BowlerID);
+
+            _context.Bowlers.Remove(deleteBowler);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ mysql/Models/EFBowlersRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mysql.Models
+{
+    public class EFBowlersRepository : IBowlersRepository
+    {
+        private BowlingDbContext _context { get; set; }
+        public EFBowlersRepository(BowlingDbContext temp)
+        {
+            _context = temp;
+        }
+        public IQueryable<Bowler> Bowlers => _context.Bowlers; 
+    }
+}
